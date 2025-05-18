e462dfce4b1a7337dd496b34913f2ead9658a771
ZhuravlevX <danzhuravlevx@gmail.com>
2025-05-19 00:54:22 +0300

    New script and icons added. Changed script social_workshop.js

---
+++ delete_sih_banners.js
@@ -0,0 +1,12 @@
+(function removeSihSponsorPanelAlways() {
+    function removeSponsorPanels() {
+        document.querySelectorAll('.sih-global.sih-sponsor-panel.header').forEach(el => el.remove());
+        document.querySelectorAll('div.sih-left-side-features-menu.sih-global').forEach(el => el.remove());
+        document.querySelectorAll('div.sih-features-button').forEach(el => el.remove());
+        document.querySelectorAll('a#sih-header-notifications.sih-header-notifications').forEach(el => el.remove());
+        document.querySelectorAll('a#sih-header-cart.sih-global-menu__icon').forEach(el => el.remove());
+    }
+    removeSponsorPanels();
+    const observer = new MutationObserver(removeSponsorPanels);
+    observer.observe(document.body, { childList: true, subtree: true });
+})();
