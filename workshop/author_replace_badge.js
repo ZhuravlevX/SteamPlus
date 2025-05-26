6f53f925836e3e0a3ceb021709bb19ecef257b65
ZhuravlevX <danzhuravlevx@gmail.com>
2025-05-27 00:29:38 +0300

    Rewritten script for any languages

--- workshop/author_replace_badge.js
+++ workshop/author_replace_badge.js
@@ -1,9 +1,5 @@
-(function replaceAuthorBadges() {
+(function() {
     document.querySelectorAll('span.commentthread_workshop_authorbadge').forEach(span => {
-        span.innerHTML = span.innerHTML
-            .replace(/\[создатель\]/gi, '★')
-            .replace(/\[Разработчик\]/gi, '★')
-            .replace(/\[author\]/gi, '★')
-            .replace(/\[developer\]/gi, '★');
+        span.innerHTML = '★';
     });
 })();
